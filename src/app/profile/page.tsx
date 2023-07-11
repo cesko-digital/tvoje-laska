1ae9f107ee5ae986102d7d347c31173de3094080
Jakub Kolebaba <jakub@kolebaba.cz>
2023-07-11 08:35:28 +0200

    Refactor profile page to use server components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,49 +1,25 @@
-"use client"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]/route"
 
-import { useSession } from 'next-auth/react'
-import { getServerSession } from 'next-auth'
-import { useEffect, useState } from 'react'
+const getProfileData = async () => {
+  // TODO: correctly type session
+  const session  = await getServerSession(authOptions)
 
-// async function getData() {
-//   const res = await fetch('https://api.example.com/...')
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
-//
-//   // Recommendation: handle errors
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data')
-//   }
-//
-//   return res.json()
-// }
+  // TODO: redirect to login if session is null
 
-async function getData() {
-  const session = await getServerSession()
-  return session;
+  const me = await fetch('https://mingly.cz/wp-json/buddypress/v1/members/me', { headers: { Authorization: session?.wpJwtToken } })
+
+  return me.json();
 }
 
-export default function Profile() {
-  const sess = useSession();
-  const jwt = sess.data?.wpJwtToken;
-  const [myData, setMyData] = useState<any>();
-
-  useEffect(() => {
-    const fetchMe = async () => {
-      const me = await fetch('https://mingly.cz/wp-json/buddypress/v1/members/me', { headers: { Authorization: jwt } })
-      setMyData(await me.json());
-    }
-
-    fetchMe();
-  }, [jwt])
+export default async function Profile () {
+  const profile = await getProfileData();
 
   return (
     <main className="w-full p-5">
-      {myData && <>
-        <img src={myData.avatar_urls.thumb}/>
-        <p className="text-white text-lg">{myData.name}</p>
-        <p className="text-white">{myData.user_login}</p>
-      </>}
+      <img src={profile.avatar_urls.thumb}/>
+      <p className="text-white text-lg">{profile.name}</p>
+      <p className="text-white">{profile.user_login}</p>
     </main>
   )
 }
